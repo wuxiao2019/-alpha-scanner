56e1a29cdf90b1765b809f3ae8e546f94a8ba78f
wuxiao2019 <58816244+wuxiao2019@users.noreply.github.com>
2026-05-02 17:44:59 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# 区块链网络配置
+BSC_RPC = "https://bsc-dataseed.binance.org/"
+ETH_RPC = "https://eth.llamarpc.com"
+
+# 筛选条件
+MAX_MARKET_CAP = 1000000  # 最大市值：100万美元
+MIN_LIQUIDITY = 30000     # 最小流动性：3万美元
+
+# Google 表格名称
+SHEET_NAME = "AlphaScanner"

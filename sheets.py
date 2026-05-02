492de1313130c63411a347ec212a141833ca005d
wuxiao2019 <58816244+wuxiao2019@users.noreply.github.com>
2026-05-02 20:30:02 +0800

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,1 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from config import *
 
+def connect_sheet():
+    creds = Credentials.from_service_account_file(
+        "credentials.json",
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+
+    client = gspread.authorize(creds)
+    return client.open_by_key(SHEET_ID)
+
+
+def write_row(spreadsheet, level, row):
+
+    if level == "HIGH":
+        sheet = spreadsheet.worksheet("HIGH")
+    elif level == "MEDIUM":
+        sheet = spreadsheet.worksheet("MEDIUM")
+    else:
+        sheet = spreadsheet.worksheet("WATCHLIST")
+
+    sheet.append_row(row)
